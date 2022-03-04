26b6663306e0f2351937566731bdba4359ec44ac
rjfmachado <rjfmachado@users.noreply.github.com>
2022-03-04 09:52:29 +0100

    updates

--- deploy/cleanup.sh
+++ deploy/cleanup.sh
@@ -1,7 +1,8 @@
 RG_NAME='ricardmabicep'
 AAD_APP_NAME='ricardmabicepregistrypush'
 
-#Create the AAD app to setup federated identity with GH
 az group delete -n $RG_NAME --yes
 AAD_APP_APPID=$(az ad app list --display-name $AAD_APP_NAME -o tsv --query [].appId)
+AAD_APP_SPID=$(az ad sp list --display-name $AAD_APP_NAME -o tsv --query [].objectId)
+az role assignment delete --assignee $AAD_APP_SPID
 az ad app delete --id $AAD_APP_APPID
--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -10,6 +10,10 @@ AAD_APP_NAME='ricardmabicepregistrypush'
 az ad app create --display-name $AAD_APP_NAME -o none
 AAD_APP_OID=$(az ad app list --display-name $AAD_APP_NAME -o tsv --query [].objectId)
 AAD_APP_APPID=$(az ad app list --display-name $AAD_APP_NAME -o tsv --query [].appId)
+AAD_TENANT_ID=$(az account show -o tsv --query tenantId)
+AZURE_SUBSCRIPTION_ID=$(az account show -o tsv --query id)
+
+
 az rest --method POST --uri "https://graph.microsoft.com/beta/applications/$AAD_APP_OID/federatedIdentityCredentials" --body "{\"name\":\"GitHub\",\"issuer\":\"https://token.actions.githubusercontent.com/\",\"subject\":\"repo:$GH_ORG/$GH_REPO:ref:refs/heads/$GH_ENTITY_NAME\",\"description\":\"GitHub\",\"audiences\":[\"api://AzureADTokenExchange\"]}" -o none
 
 #
@@ -23,11 +27,9 @@ az ad sp create --id $AAD_APP_APPID -o none
 AAD_APP_SPID=$(az ad sp list --display-name $AAD_APP_NAME -o tsv --query [].objectId)
 
 gh secret set AZURE_CLIENT_ID --body "$AAD_APP_SPID" --repo $GH_ORG/$GH_REPO
+gh secret set AZURE_SUBSCRIPTION_ID --body "$AZURE_SUBSCRIPTION_ID" --repo $GH_ORG/$GH_REPO
+gh secret set AZURE_TENANT_ID --body "$AAD_TENANT_ID" --repo $GH_ORG/$GH_REPO
 
 #Setup the ACR and allow acrpush RBAC access from the GH repo
 az deployment sub create --template-file ~/dev/github.com/rjfmachado/bicepregistry/infra/bicepacr.bicep --location westeurope -o table --parameters name=$RG_NAME location=$LOCATION principalId=$AAD_APP_SPID roleDefinitionIdOrName=acrPush --query outputs
-az role assignment create --assignee $AAD_APP_SPID --role "Reader"
-#Cleanup
-#az group delete -n $RG_NAME --yes
-#AAD_APP_APPID=$(az ad app list --display-name $AAD_APP_NAME -o tsv --query [].appId)
-#az ad app delete --id $AAD_APP_APPID
+az role assignment create --assignee $AAD_APP_SPID --role "Contributor" -o none
