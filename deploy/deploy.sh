8ae309161f56cd9bda0daf9396333d4ce96f909c
rjfmachado <rjfmachado@users.noreply.github.com>
2022-04-11 17:25:06 +0200

    update deploy script

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -25,14 +25,18 @@ az rest --method POST --uri "https://graph.microsoft.com/beta/applications/$AAD_
 az ad sp create --id $AAD_ACRPUSH_APP_APPID -o none
 AAD_ACRPUSH_APP_SPID=$(az ad sp list --display-name $AAD_ACRPUSH_APP_NAME -o tsv --query [].objectId)
 
+# Create the Target Resource Group
+az group create --name $AZURE_RG_NAME --location $AZURE_LOCATION -o none
+
 # Allow the Deploy app Owner access to the Resource Group
-az role assignment create --assignee $AAD_DEPLOY_APP_SPID --role "Owner" --resource-group $AZURE_RG_NAME
+az role assignment create --assignee $AAD_DEPLOY_APP_SPID --role "Owner" --resource-group $AZURE_RG_NAME -o none
 
 # Allow the ACR Push app read access to the ACR
-az role assignment create --assignee $AAD_ACRPUSH_APP_SPID --role "Reader" --resource-group $AZURE_RG_NAME
+az role assignment create --assignee $AAD_ACRPUSH_APP_SPID --role "Reader" --resource-group $AZURE_RG_NAME -o none
 
 #Update the Registry, TenantId, SubscriptionId and AppId's in GitHub
 gh secret set AZURE_ACRPUSH_CLIENT_ID --body "$AAD_ACRPUSH_APP_APPID" --repo $GH_ORG/$GH_REPO
 gh secret set AZURE_DEPLOY_CLIENT_ID --body "$AAD_ACRPUSH_APP_APPID" --repo $GH_ORG/$GH_REPO
 gh secret set AZURE_SUBSCRIPTION_ID --body "$AZURE_SUBSCRIPTION_ID" --repo $GH_ORG/$GH_REPO
 gh secret set AZURE_TENANT_ID --body "$AAD_TENANT_ID" --repo $GH_ORG/$GH_REPO
+gh secret set AZURE_RG_NAME --body "$AZURE_RG_NAME" --repo $GH_ORG/$GH_REPO
