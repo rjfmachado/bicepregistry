b72dd4e48c14e3971e23a72a0f03b7b3bcd216ee
rjfmachado <rjfmachado@users.noreply.github.com>
2022-04-11 18:58:06 +0200

    update README

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -34,11 +34,10 @@ az role assignment create --assignee $AAD_DEPLOY_APP_SPID --role "Owner" --resou
 # Allow the ACR Push app read access to the ACR
 az role assignment create --assignee $AAD_ACRPUSH_APP_SPID --role "Reader" --resource-group $AZURE_RG_NAME -o none
 
-#Update the Registry, TenantId, SubscriptionId and AppId's in GitHub
+# Update the Registry, TenantId, SubscriptionId and AppId's in GitHub
 gh secret set AZURE_ACRPUSH_CLIENT_ID --body "$AAD_ACRPUSH_APP_APPID" --repo $GH_ORG/$GH_REPO
 gh secret set AZURE_DEPLOY_CLIENT_ID --body "$AAD_ACRPUSH_APP_APPID" --repo $GH_ORG/$GH_REPO
 gh secret set AZURE_SUBSCRIPTION_ID --body "$AZURE_SUBSCRIPTION_ID" --repo $GH_ORG/$GH_REPO
 gh secret set AZURE_TENANT_ID --body "$AAD_TENANT_ID" --repo $GH_ORG/$GH_REPO
 gh secret set AZURE_RG_NAME --body "$AZURE_RG_NAME" --repo $GH_ORG/$GH_REPO
-gh secret set AZURE_RG_NAME --body "$AZURE_ACR_NAME" --repo $GH_ORG/$GH_REPO
-
+gh secret set AZURE_ACR_NAME --body "$AZURE_ACR_NAME" --repo $GH_ORG/$GH_REPO
