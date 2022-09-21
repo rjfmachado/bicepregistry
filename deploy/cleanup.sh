f7de07001f0a7abc1b2bf158673a3b366181935f
Ricardo Machado <rjfmachado@users.noreply.github.com>
2022-09-21 13:32:34 +0000

    update to support azure cli 2.40

--- deploy/cleanup.sh
+++ deploy/cleanup.sh
@@ -1,8 +1,15 @@
-RG_NAME='ricardmabicep'
-AAD_APP_NAME='bicepregistrypush'
-
+RG_NAME='bicepregistry'
 az group delete -n $RG_NAME --yes
+
+AAD_APP_NAME='bicepregistryacrpush'
 AAD_APP_APPID=$(az ad app list --display-name $AAD_APP_NAME -o tsv --query [].appId)
-AAD_APP_SPID=$(az ad sp list --display-name $AAD_APP_NAME -o tsv --query [].objectId)
+AAD_APP_SPID=$(az ad sp list --display-name $AAD_APP_NAME -o tsv --query [].id)
 az role assignment delete --assignee $AAD_APP_SPID
 az ad app delete --id $AAD_APP_APPID
+
+AAD_APP_NAME='bicepregistrydeploy'
+AAD_APP_APPID=$(az ad app list --display-name $AAD_APP_NAME -o tsv --query [].appId)
+AAD_APP_SPID=$(az ad sp list --display-name $AAD_APP_NAME -o tsv --query [].id)
+az role assignment delete --assignee $AAD_APP_SPID
+az ad app delete --id $AAD_APP_APPID
+
